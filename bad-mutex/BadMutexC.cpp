9b2ab6f15b90f28abd59f6a09410a95047cf803c
torque <torque@1>
2015-02-27 11:12:54 -0800

    BadMutex: remove debug logging.

--- bad-mutex/BadMutexC.cpp
+++ bad-mutex/BadMutexC.cpp
@@ -1,14 +1,11 @@
 #include "BadMutex.hpp"
 #include "BadMutexC.h"
-#include <cstdio>
 
 extern "C" {
 	EXPORT void lock( void ) {
 		BadMutex::getInstance().lock();
-		puts("locked");
 	}
 	EXPORT void unlock( void ) {
 		BadMutex::getInstance().unlock();
-		puts("unlocked");
 	}
 }
