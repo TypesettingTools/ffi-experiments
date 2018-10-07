75c527fa8eef408abc4d62f74911bc6be8017cb0
torque <torque@users.noreply.github.com>
2018-10-06 23:15:03 -0700

    meson: cleanup and improvements.
    This commit does several things:
    - brings back the Makefile so people do not have to install meson
    - moves DownloadManager into a directory name that actually makes sense
    - hacks the meson build system to add a target for generating the lua scripts
    - modifies BuildLua.sh to support the hacked meson build system
    - removes the old Visual Studio solutions (they have been obsoleted by meson)
    - makes some minor corrections to the readme.

--- BuildLua.sh
+++ BuildLua.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
-if [ -z $3 ]; then
-	echo "Usage: $0 dir-name ModuleName output.lua"
-	exit 1
+if [ -z $2 ]; then
+	echo "Usage: $0 input_file output_file [input2 output2 ...]"
+	echo "e.g.: $0 'threaded-libcurl/DownloadManager.moon' 'DownloadManager.lua'"
+	exit 0
 fi
 
-cd $1
-headerInput="$2C.h"
-headerOutput="$2C.processed.h"
-moonInput="$2.moon"
-moonOutput="$2.processed.moon"
-luaOutput="$3"
+while [[ "$#" -ge 2 ]]; do
+	moonInput="$1"
+	moonInputBase=`basename "$moonInput"`
 
-# clean up excess newlines for aesthetics.
-cpp -P "$headerInput" | perl -pe "s/^\n$//g" > "$headerOutput"
-# drop the processed header into the moonscript file.
-cat "$moonInput" | perl -pe "s/___INCLUDE___/`cat $headerOutput`/" > "$moonOutput"
-# compile the moonscript file.
-moonc -o "../$luaOutput" "$moonOutput" 2> /dev/null
-# clean up intermediates.
-rm "$headerOutput" "$moonOutput"
+	moduleName=${moonInputBase%.moon}
+	sourceDir=`dirname "$moonInput"`
+
+	headerInput="$sourceDir/${moduleName}C.h"
+	headerOutput="$sourceDir/${moduleName}C.processed.h"
+	moonOutput="${moonInput%.moon}.processed.moon"
+	luaOutput="$2"
+
+	if [[ -r $headerInput ]]; then
+		# clean up excess newlines for aesthetics.
+		cpp -P "$headerInput" | perl -pe "s/^\n$//g" > "$headerOutput"
+		# drop the processed header into the moonscript file.
+		cat "$moonInput" | perl -pe "s/___INCLUDE___/`cat "$headerOutput"`/" > "$moonOutput"
+		# clean up preprocessed header
+		# compile the moonscript file.
+		moonc -o "$luaOutput" "$moonOutput" 2>/dev/null
+		rm "$headerOutput" "$moonOutput"
+	else
+		moonc -o "$luaOutput" "$moonInput" 2>/dev/null
+	fi
+
+	shift 2
+done
